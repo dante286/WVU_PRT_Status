e89f784c960ba28487370fcd563fc8384e67f663
dante286 <matt.nisan@gmail.com>
2018-08-19 18:20:56 +0000

    Initial commit with status script

--- prtStatus.sh
+++ prtStatus.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 wget -o ./statuses/index.html http://prtstatus.wvu.edu/api/$(date +%s)/?format=json
 
-old_status="$(jq .status "index.html?format=json")"
-new_status="$(jq .status "index.html?format=json.1")"
-if ($new_status = $old_status); then
+old_status="$(jq -r .status "index.html?format=json")"
+new_status="$(jq -r .status "index.html?format=json.1")"
+if [ "$new_status" = "$old_status" ];
+then
 	echo "Status has not changed"
+	rm index.html?format=json.*
+	exit 0
 else
 	echo "status has changed to $new_status"
+	mv index.html?format=json.1 index.html?format=json
+	return $new_status
 fi
-
-echo $old_status
-echo $new_status
-
-rm index.html?format=json.*
