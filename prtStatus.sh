f12d004d25a9b20c3f8b10d52f3137bdb8815acb
dante286 <matt.nisan@gmail.com>
2018-08-20 00:02:38 +0000

    Updated script to no longer use jq

--- prtStatus.sh
+++ prtStatus.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 wget -o ./statuses/index.html http://prtstatus.wvu.edu/api/$(date +%s)/?format=json
 
-old_status="$(jq -r .status "index.html?format=json")"
-new_status="$(jq -r .status "index.html?format=json.1")"
-if [ "$new_status" = "$old_status" ];
+diff_status="$(diff "index.html?format=json" "index.html?format=json.1")"
+
+if [ "$diff_status" ]
 then
-	echo "Status has not changed"
-	rm index.html?format=json.*
+	echo "Status has changed"
+	echo "$diff_status"
+	mv index.html?format=json.1 index.html?format=json
 	exit 0
 else
-	echo "status has changed to $new_status"
-	mv index.html?format=json.1 index.html?format=json
-	return $new_status
+	echo "Status has not changed"
+	rm index.html?format=json.*
+	return 0
 fi
